57c085b51b398ff65f82d54f67e1d8d9808822b9
kemal sahin <kemalshnn267@gmail.com>
2023-10-02 12:37:23 +0300

    katmanlar , sınıflar , paketler  ve veri tabanı sınıfı oluşturuldu ardından abstrac sınıfının içindeki fonksiyonlar interfaceler tanımlandı entityframework sınıflarıda oluşturuldu

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,38 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-LKHTLE8\\SQLEXPRESS; database=TraversalDb; integrated security=true");
+            
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> About2s { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Featureds { get; set; }
+        public DbSet<Feature2> Featured1s { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<NewsLetter> NewsLetters { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        //public DbSet<Comment> Comments { get; set; }
+        //public DbSet<Reservation> Reservations { get; set; }
+        //public DbSet<ContactUs> tblContactUs { get; set; }
+        //public DbSet<Announcement> Announcements { get; set; }
+        //public DbSet<Account> Accounts { get; set; }
+        //public DbSet<City> Cities { get; set; }
+    }
+}
+
