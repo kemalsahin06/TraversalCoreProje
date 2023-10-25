300bab79064d887678a90b23e17a2fd07c3cdb94
kemal sahin <kemalshnn267@gmail.com>
2023-10-25 17:41:05 +0300

    64 bitti

--- TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using DocumentFormat.OpenXml.Wordprocessing;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using TraversalCoreProje.Models;
@@ -8,6 +11,12 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
     [Area("Admin")]
     public class CityController : Controller
     {
+        private readonly IDestinationService _destinationService;
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,8 +25,25 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
 
         public IActionResult CityList()
         {
-            var jsonCity = JsonConvert.SerializeObject(cities);
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
             return Json(jsonCity);
+            
+        }
+
+        public IActionResult GetByID(int DestinationID)
+        {
+            var values = _destinationService.TGetByID(DestinationID);
+            var jsonValues = JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
+
+        [HttpPost]
+        public IActionResult AddCityDestination(EntityLayer.Concrete.Destination destination)
+        {
+            destination.Status = true;
+            _destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(values);
         }
 
 
@@ -46,6 +72,8 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
                 CityCountry = "İngiltere"
             }
         };
+
+       
     }
 
 
