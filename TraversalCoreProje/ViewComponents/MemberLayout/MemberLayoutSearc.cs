be79c2cf8fbc849cc384da42bf697efebbec4666
kemal sahin <kemalshnn267@gmail.com>
2023-11-26 13:19:40 +0300

    sil

---
+++ TraversalCoreProje/ViewComponents/MemberLayout/MemberLayoutSearc.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.MemberLayout
+{
+    public class MemberLayoutSearc :  ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
