5d8e79f0d61796640f63ac037385b3fc24124f05
kemal sahin <kemalshnn267@gmail.com>
2025-05-12 17:08:03 +0300

    son commit

--- TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -1,19 +1,35 @@
-﻿using BusinessLayer.Concrete;
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
     [Area("Member")]
     [AllowAnonymous]
-    public class DestinationController : Controller
+	[Route("Member/[controller]/[action]")] // burda aşşagıdaki yönlendirme işleminde bunu yazmadan diger sayfalara geçiş saglamadı
+
+	public class DestinationController : Controller
     {
         DestinationManager dm = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
         {
             var values = dm.TGetList();
             return View(values);
+        }
+
+
+        public IActionResult GetCitiesSearchByName(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var values = from x in dm.TGetList() select x;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                values = values.Where(y => y.CityName.ToLower().Contains(searchString.ToLower()));
+            }
+            return View(values.ToList());
         }
     }
 }
---
+++ TraversalCoreProje/Controllers/PasswordChangeController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper.Internal;
+using EntityLayer.Concrete;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+using System.Threading.Tasks;
+using TraversalCoreProje.Areas.Admin.Models;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Controllers
+{
+	
+	public class PasswordChangeController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public PasswordChangeController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpGet]
+		public IActionResult ForGetPassword()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ForGetPassword(ForgetPasswordViewModel forgetPasswordViewModel)
+		{
+			var user = await _userManager.FindByEmailAsync(forgetPasswordViewModel.Mail);
+			string passwordResetToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+			var passwordResetTokenLink = Url.Action("ResetPassword", "PasswordChange", new
+			{
+				userId = user.Id,
+				token = passwordResetToken
+			}, HttpContext.Request.Scheme);
+
+			MimeMessage mimeMessage = new MimeMessage();
+
+			MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "traversalcore2@gmail.com");
+			mimeMessage.From.Add(mailboxAddressFrom);  // burda göndericek kişinin bilgilerini aldım
+
+
+			MailboxAddress mailboxAddressTo = new MailboxAddress("User", forgetPasswordViewModel.Mail);
+			mimeMessage.To.Add(mailboxAddressTo);
+			// burda alacak kişinin bilgilerini yazdım
+
+			var bodyBuilder = new BodyBuilder();
+			bodyBuilder.TextBody = passwordResetTokenLink;
+
+
+			mimeMessage.Subject = "şifre Degişiklik Paneli"; // konuyu aldık
+
+			SmtpClient client = new SmtpClient();
+			client.Connect("smtp.gmail.com", 587, false);
+			client.Authenticate("traversalcore2@gmail.com", "123456aA-");
+
+			client.Send(mimeMessage);
+			client.Disconnect(true);
+
+
+			return View();
+		}
+
+        [HttpGet]
+        public IActionResult ResetPassword(string userid, string token)
+        {
+            TempData["userid"] = userid;
+            TempData["token"] = token;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel resetPasswordViewModel)
+        {
+            var userid = TempData["userid"];
+            var token = TempData["token"];
+            if (userid == null || token == null)
+            {
+                //hata mesajı
+            }
+            var user = await _userManager.FindByIdAsync(userid.ToString());
+            var result = await _userManager.ResetPasswordAsync(user, token.ToString(), resetPasswordViewModel.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+            return View();
+        }
+
+
+    }
+}
